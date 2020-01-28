ba9b39034db609af280baf948ef272f9ceecfbd4
Manuel Pfemeter <manne@users.noreply.github.com>
2020-01-28 21:33:14 +0100

    Follow coding guide lines

--- src/CodeGeneration.Roslyn.Tests.Generators/NullableGenerator.cs
+++ src/CodeGeneration.Roslyn.Tests.Generators/NullableGenerator.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MS-PL license. See LICENSE.txt file in the project root for full license information.
+
 namespace CodeGeneration.Roslyn.Tests.Generators
 {
+    using System;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.CodeAnalysis;
--- src/CodeGeneration.Roslyn.Tests/OtherFolder/NullableGeneratorTests.cs
+++ src/CodeGeneration.Roslyn.Tests/OtherFolder/NullableGeneratorTests.cs
@@ -1,4 +1,7 @@
-﻿namespace CodeGeneration.Roslyn.Tests.OtherFolder
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MS-PL license. See LICENSE.txt file in the project root for full license information.
+
+namespace CodeGeneration.Roslyn.Tests.OtherFolder
 {
     using System;
     using System.Diagnostics;
