46ffd8d85d0396db7ee7f66fdd3003eadc60847c
Manuel Pfemeter <manne@users.noreply.github.com>
2020-01-28 20:40:47 +0100

    Add example for a generator respecting nullability

---
+++ src/CodeGeneration.Roslyn.Tests.Generators/NullableGenerator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+namespace CodeGeneration.Roslyn.Tests.Generators
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Validation;
+    using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+    public class NullableGenerator : ICodeGenerator
+    {
+        public NullableGenerator(AttributeData attributeData)
+        {
+            Requires.NotNull(attributeData, nameof(attributeData));
+        }
+
+        public Task<SyntaxList<MemberDeclarationSyntax>> GenerateAsync(TransformationContext context, IProgress<Diagnostic> progress, CancellationToken cancellationToken)
+        {
+            var results = List<MemberDeclarationSyntax>();
+            MemberDeclarationSyntax copy = null;
+            if (context.ProcessingNode is ClassDeclarationSyntax applyToClass)
+            {
+                var nullableContext = context.SemanticModel.GetNullableContext(applyToClass.SpanStart);
+                var isNullableEnabled = nullableContext == NullableContext.Enabled;
+                var method = MethodDeclaration(
+                        MakeNullableIfEnabled(
+                            PredefinedType(
+                                Token(SyntaxKind.StringKeyword)), isNullableEnabled),
+                        Identifier("DoSomething"))
+                    .WithModifiers(
+                        TokenList(
+                            Token(SyntaxKind.PublicKeyword)))
+                    .WithParameterList(
+                        ParameterList(
+                            SingletonSeparatedList(
+                                Parameter(
+                                        Identifier("value"))
+                                    .WithType(
+                                        MakeNullableIfEnabled(
+                                            PredefinedType(
+                                                Token(SyntaxKind.StringKeyword)), isNullableEnabled)))))
+                    .WithBody(
+                        Block(
+                            SingletonList<StatementSyntax>(
+                                ReturnStatement(
+                                    IdentifierName("value")))));
+                if (isNullableEnabled)
+                {
+                    method = method.WithLeadingTrivia(TriviaList(Trivia(
+                            NullableDirectiveTrivia(
+                                Token(SyntaxKind.EnableKeyword),
+                                true))))
+                        .WithTrailingTrivia(TriviaList(Trivia(
+                            NullableDirectiveTrivia(
+                                Token(SyntaxKind.RestoreKeyword),
+                                true))));
+                }
+
+                copy = ClassDeclaration(applyToClass.Identifier)
+                    .WithModifiers(applyToClass.Modifiers)
+                    .AddMembers(method);
+            }
+
+            if (copy != null)
+            {
+                results = results.Add(copy);
+            }
+
+            return Task.FromResult(results);
+        }
+
+        public static TypeSyntax MakeNullableIfEnabled(TypeSyntax typeSyntax, bool isEnabled)
+        {
+            return isEnabled ? NullableType(typeSyntax) : typeSyntax;
+        }
+    }
+}
---
+++ src/CodeGeneration.Roslyn.Tests/OtherFolder/NullableGeneratorTests.cs
@@ -0,0 +1,48 @@
+﻿namespace CodeGeneration.Roslyn.Tests.OtherFolder
+{
+    using System;
+    using System.Diagnostics;
+    using CodeGeneration.Roslyn.Tests.Generators;
+    using Xunit;
+
+    public class NullableGeneratorTests
+    {
+#nullable enable
+        [Fact]
+        public void TestForWhichNullableIsEnabled()
+        {
+            var instance = new NullableEnabled();
+            _ = instance.DoSomething("blub");
+        }
+
+#nullable disable
+        [Fact]
+        public void TestForWhichNullableIsDisabled()
+        {
+            var instance = new NullableDisabled();
+            _ = instance.DoSomething("blub");
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    [CodeGenerationAttribute(typeof(NullableGenerator))]
+    [Conditional("CodeGeneration")]
+    public class NullableGeneratorAttribute : Attribute
+    {
+        // nothing to do here
+    }
+
+#nullable enable
+    [NullableGenerator]
+    public partial class NullableEnabled
+    {
+        // an method string? DoSomething(string?) will be generated
+    }
+
+#nullable disable
+    [NullableGenerator]
+    public partial class NullableDisabled
+    {
+        // an method string DoSomething(string) will be generated
+    }
+}
